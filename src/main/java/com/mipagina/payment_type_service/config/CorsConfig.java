d3cd05e4fd4500ce6e1c85be8db0e6dcd4fe9cc8
CafeyTigrillo <cafeytigrillomagic@gmail.com>
2025-02-11 15:01:40 -0500

    🐳 Add Dockerfile for payment-type-service

--- src/main/java/com/mipagina/payment_type_service/config/CorsConfig.java
+++ src/main/java/com/mipagina/payment_type_service/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Cambia esto según el frontend
+                        .allowedOrigins("http://ec2-13-216-183-248.compute-1.amazonaws.com:3000") // Cambia esto según el frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
